c54d907b1c3d7f589d99dd9ba7d740ec480be488
Ryan Payne <ryan.payne@daveramsey.com>
2018-12-26 17:49:13 -0600

    Accept custom selector

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -10,30 +10,54 @@ jest.mock('mathjs', () => {
 describe('InlineCalculator', () => {
   let input;
 
-  beforeEach(() => {
-    document.body.innerHTML = '<input type="text" id="inline-calculator">';
-    new InlineCalculator();
-    input = document.getElementById('inline-calculator');
-    input.value = '2 + 2';
+  describe('Default config', () => {
+    beforeEach(() => {
+      document.body.innerHTML = '<input type="text" id="inline-calculator">';
+      new InlineCalculator();
+      input = document.querySelector('#inline-calculator');
+      input.value = '2 + 2';
+    });
+
+    it('should do nothing', () => {
+      input.dispatchEvent(new KeyboardEvent('keydown', {
+        key: 'f',
+      }));
+
+      expect(math.eval).not.toBeCalled();
+      expect(input.value).toBe('2 + 2');
+    });
+
+    it('should update input value', () => {
+      math.eval.mockReturnValueOnce(4);
+
+      input.dispatchEvent(new KeyboardEvent('keydown', {
+        key: 'Enter',
+      }));
+
+      expect(math.eval).toBeCalledWith('2 + 2');
+      expect(input.value).toBe('4');
+    });
   });
 
-  it('should do nothing', () => {
-    input.dispatchEvent(new KeyboardEvent('keydown', {
-      key: 'f',
-    }));
+  describe('Custom selector', () => {
+    beforeEach(() => {
+      document.body.innerHTML = '<input type="text" class="inline-calculator">';
+      new InlineCalculator({
+        selector: '.inline-calculator'
+      });
+      input = document.querySelector('.inline-calculator');
+      input.value = '2 + 2';
+    });
 
-    expect(math.eval).not.toBeCalled();
-    expect(input.value).toBe('2 + 2');
-  });
+    it('should accept a custom selector', () => {
+      math.eval.mockReturnValueOnce(4);
 
-  it('should update input value', () => {
-    math.eval.mockReturnValueOnce(4);
+      input.dispatchEvent(new KeyboardEvent('keydown', {
+        key: 'Enter',
+      }));
 
-    input.dispatchEvent(new KeyboardEvent('keydown', {
-      key: 'Enter',
-    }));
-
-    expect(math.eval).toBeCalledWith('2 + 2');
-    expect(input.value).toBe('4');
+      expect(math.eval).toBeCalledWith('2 + 2');
+      expect(input.value).toBe('4');
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import * as math from 'mathjs';
 
 export default class InlineCalculator {
-  constructor() {
-    let input = document.getElementById('inline-calculator');
+  constructor(userConfig) {
+    let input;
+    const defaultConfig = {
+      selector: '#inline-calculator'
+    };
+    const config = Object.assign(defaultConfig, userConfig);
+
+    input = document.querySelector(config.selector);
     input.addEventListener('keydown', (event) => {
       if (event.key !== 'Enter') return;
 

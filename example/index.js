dd5479c5990ec269d6c7224378c7e63992450bcb
Ryan Payne <ryan.payne@daveramsey.com>
2019-01-12 17:59:30 -0600

    Simplify example

--- example/index.js
+++ example/index.js
@@ -1,10 +1,7 @@
 import InlineCalculator from '../src/index';
 
-new InlineCalculator();
-
-var calc2 = new InlineCalculator({
-    selector: '#calc2',
-    onError: function (err) {
-        console.log('passed error', err)
-    }
+new InlineCalculator({
+  onError: (error) => {
+    alert(error);
+  }
 });
